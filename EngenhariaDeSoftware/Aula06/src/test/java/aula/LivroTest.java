fc87294bfaed2e49d1ecdb789ee0a6256a167c11
marvintomizawa <marvintomizawa@hotmail.com>
2019-03-12 20:34:20 -0300

    [AULA] Aula do dia 12/03/2019

---
+++ EngenhariaDeSoftware/Aula06/src/test/java/aula/LivroTest.java
@@ -0,0 +1,12 @@
+package aula;
+
+import org.junit.Test;
+
+public class LivroTest {
+
+	@Test
+	public void testarInstanciasVersusReferencias() {
+		Livro umlApplied = new Livro();
+		Livro bibliaSagrada = new Livro();
+	}
+}
